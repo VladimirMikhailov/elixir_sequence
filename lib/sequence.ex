d7037f01a1020d2c58df67275ed483a41c24c828
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2016-04-05 15:27:21 +0300

    Add moduledoc

--- lib/sequence.ex
+++ lib/sequence.ex
@@ -1,4 +1,16 @@
 defmodule Sequence do
+  @moduledoc """
+    Functional look and say implementation
+
+    ## Examples
+
+      iex> Sequence.calc([])
+      []
+
+      iex> Sequence.calc([1, 1])
+      [2, 1]
+  """
+
   def calc([]), do: []
   def calc([first | other]), do: calc(other, first, 1)
 
